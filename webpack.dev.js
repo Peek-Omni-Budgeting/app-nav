782dff608903de40d696f5e0bed94e9aaf6a3469
Nathan Peek <nmp14fsu@gmail.com>
2022-10-30 18:44:33 -0400

    setup dev and prod webpack

---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+const targetPort = argv['port'] || 3000;
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  optimization: {
+    minimize: false,
+  },
+
+  cashe: true,
+
+  output: {
+    publicPath: `http://localhost:${targetPort}/`,
+  },
+
+  devServer: {
+    static: path.json(__dirname, 'public'),
+    port: targetPort,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Content-Type, Authorization',
+    }
+  }
+});
